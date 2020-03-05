044e619b30ed350cbecebe4f3d43e02c459cc6ed
sh3riff <contactme@sh3riff.me>
2020-03-05 17:59:10 +0100

    form completed with temporal form processor

--- order.js
+++ order.js
@@ -20,24 +20,38 @@
 }());
 
 
+let linkArray = ['link1'];
+let descArray = ['desc1'];
+
 (function addOrder (){
     document.querySelector('button.add').addEventListener('click', e => {
         e.preventDefault();
+        const rand = Math.floor(1000 * Math.random());
         const section = document.querySelector('.orderList');
         const div = document.createElement('div');
         const url = document.createElement('input');
         url.type = "url";
+        url.name = `link${rand}`;
+        url.id = 'link';
         url.placeholder = "Input order link";
         const close = document.createElement('span');
         close.innerText = '✖';
         close.className = 'delOrder'
         const desc = document.createElement('textarea');
+        desc.name = `desc${rand}`;
+        desc.id = 'desc';
         desc.placeholder="additional information";
 
         div.appendChild(url);
         div.appendChild(close)
         div.appendChild(desc);
         section.appendChild(div);
+
+        linkArray.push(url.name);
+        descArray.push(desc.name);
+        
+        // console.log(linkArray);
+        // console.log(descArray);
     })
 }());
 
@@ -45,6 +59,14 @@
     let target = document.querySelector('section.orderList');
     target.addEventListener('click', e => {
         if(e.target.className === 'delOrder'){
+            // console.log(e.target.parentElement.querySelector('#link').name);
+            // console.log(e.target.parentElement.querySelector('#desc').name);
+            let linkIndex = linkArray.indexOf(`${e.target.parentElement.querySelector('#link').name}`);
+            let descIndex = descArray.indexOf(`${e.target.parentElement.querySelector('#desc').name}`);
+            // console.log(linkIndex);
+            // console.log(descIndex);
+            linkArray.splice(`${linkIndex}`, 1);
+            descArray.splice(`${descIndex}`, 1);
             const p = e.target.parentElement;
             p.parentNode.removeChild(p);
         }
